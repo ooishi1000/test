8cce2a7bfe02143eea8cbf49d09d7c8e7eb530f2
ooishi1000 <karugon64@gmail.com>
2023-07-20 09:53:21 +0900

    C#問題集、連想配列

---
+++ test.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var Object = new Dictionary<string, string>()
+        {
+            {"Kyoko", "B"},
+            {"Rio", "O"},
+            {"Tsubame", "AB"},
+            {"KurodaSensei", "A"},
+            {"NekoSensei", "A"}
+        };
+        
+        foreach(KeyValuePair<string, string>item in Object) {
+            Console.WriteLine("{0} {1}", item.Key, item.Value);
+        }
+        
+    }
+}
